8d7702659713e8ac801593a44adc429da95f812f
jewelcodes <omarmelghoul01@gmail.com>
2024-04-02 23:06:50 -0400

    login button

---
+++ public/assets/js/wrapper/api.js
@@ -0,0 +1,50 @@
+const api = "http://127.0.0.1:5000/";   // TODO
+
+async function request(uri, parameters=null, type="GET", file=false) {
+    let path = api+uri;
+    let parsedParams = "";
+
+    if(parameters != null) {
+        keys = Object.keys(parameters);
+        values = Object.values(parameters);
+        
+        for(let i = 0; i < keys.length; i++) {
+            parsedParams += keys[i] + "=" + values[i] + "&";
+        }
+    }
+
+    if(type == "GET" && parameters != null) {
+        path += "?" + parsedParams;
+    }
+
+    if(!file) {
+        const response = await fetch(path, {
+            method: type,
+            mode: type == "GET" ? "cors" : "no-cors",
+            cache: "no-cache",
+            headers: type == "GET" ? {} : { "Content-Type": (file == false) ? "application/x-www-form-urlencoded" : "multipart/form-data" },
+            body: type == "GET" ? null : parsedParams   // TODO
+        });
+
+        if(type == "GET") {
+            const output = await response.json();
+            return output;
+        }
+    } else {
+        // FILE UPLOAD
+        var data = new FormData();
+        data.append("data", parameters.data);
+        data.append("id", getCookie("id"));
+
+        const response = await fetch(path, {
+            method: "POST",
+            //headers: { "Content-Type" : "multipart/form-data"},   // wtf was i thinking here
+            body: data
+        });
+    }
+}
+
+function login() {
+    window.location.replace(api + "weblogin");
+    return false;
+}
